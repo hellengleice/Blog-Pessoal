ce929ff6cf12052a81814df498d266504263f7e8
hellengleice <hellengleice.97@gmail.com>
2025-03-11 17:01:02 -0300

    implementação do JWT e serviço de usuário

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -20,77 +20,75 @@ import jakarta.validation.constraints.Size;
 @Table(name = "tb_postagens")
 public class Postagem {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		
-	@NotBlank(message = "O atributo titulo é obrigatorio")
-	@Size(min = 5, max = 100, message = "O titulo tem que ser maior que 5 e menor que 100")
-		private String titulo;
-		
-	@NotBlank(message = "O atributo texto é obrigatorio")
-	@Size(min = 5, max = 100, message = "O texto tem que ser maior que 5 e menor que 100")
-		private String texto;	
-
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
 	@UpdateTimestamp
-		private LocalDateTime data;
+	private LocalDateTime data;
 	
-	@ManyToOne //classe postagem muitos : classe tema um
+	@ManyToOne
 	@JsonIgnoreProperties("postagem")
-	private Tema tema; //adicionado o objeto tema (id, descricao)
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-
-	public String getTexto() {
-		return texto;
-	}
-
-
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+	private Tema tema;
 	
-	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
-	public LocalDateTime getData() {
-		return data;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
+    public String getTitulo() {
+        return this.titulo;
+    }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 
 	public Tema getTema() {
 		return tema;
 	}
 
-
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
-		
-	
-	
+
+	public Usuario getUsuario() {
+		return usuario;
 	}
 
-
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    
+}
---
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -0,0 +1,77 @@
+package com.generation.blogpessoal.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtService {
+
+	//constante 
+	public static final String SECRET = "df3906d090957ef32cdc0afbadcc75cc70c043e34c62b83308a96df8e1487c44";
+
+	//chave secret foi codificada base64
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignKey()).build()
+				.parseClaimsJws(token).getBody();
+	}
+
+	//extrair os claims -> informações token
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());//validade token
+	}
+
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+	private String createToken(Map<String, Object> claims, String userName) {
+		return Jwts.builder()
+					.setClaims(claims)
+					.setSubject(userName)
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+	
+	/*
+	 * data atual + 100 *60 *60 => 1h
+	 */
+
+	public String generateToken(String userName) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userName);
+	}
+
+}
